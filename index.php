7c3bf96ef10fbf9916c947ff15c724db1c1cd580
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-16 14:38:34 +0100

    Inizializzato progetto

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $message = 'hello';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="dist/app.css">
+    </head>
+    <body>
+        <h1>
+            <?php echo $message; ?>
+        </h1>
+    </body>
+</html>
